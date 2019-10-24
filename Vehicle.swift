c7c281ed70436c25bae6a34e8c435d22cff18dc4
akash13deep <56893097+akash13deep@users.noreply.github.com>
2019-10-24 15:52:13 -0400

    Update Vehicle.swift

--- Vehicle.swift
+++ Vehicle.swift
@@ -1,75 +1,109 @@
-//
-//  Vehicle.swift
-//  Rally
-//
-//  Created by JASI on 23/10/19.
-//  Copyright © 2019 JASI. All rights reserved.
-//
-
 import Foundation
 
-protocol VehicleProtocol{
+                                                //creating a protocol called VehicleProtocol
+
+protocol VehicleProtocol
+{
+    
+                                                //declaring data members
+    
     var name: String { get }
     var maxSpeed: Double { get }
     var weight: Int { get }
     var fuelLevel: Int { get }
 }
 
-class Vehicle: VehicleProtocol{
+                                                 //super class Vehicle implements protocol VehicleProtocol
+
+class Vehicle: VehicleProtocol
+{
+                                                //declaring data members
     var name: String
     var maxSpeed: Double
     var weight: Int
     var fuelLevel: Int
     
-    init(){
+                                                //default initializer
+    
+    init()
+    {
         name = "Anonym"
         maxSpeed = 130
         weight = 1000
         fuelLevel = 0
     }
     
-    init(name: String,maxSpeed: Double,weight: Int, fuelLevel:Int) {
+                                                //initializer with parameters
+    
+    init(name: String,maxSpeed: Double,weight: Int, fuelLevel:Int)
+    {
         self.name = name
         self.weight = weight
         self.maxSpeed = maxSpeed
         self.fuelLevel = fuelLevel
     }
-    
-    func getName() -> String{
+                                                //getter
+    func getName() -> String
+    {
         return self.name
     }
     
-    func getMaxSpeed() -> Double{
+    func getMaxSpeed() -> Double
+    {
         return self.maxSpeed
     }
     
-    func getWeight() -> Int{
+    func getWeight() -> Int
+    {
         return self.weight
     }
     
-    func getFuelLevel() -> Int{
+    func getFuelLevel() -> Int
+    {
         return self.fuelLevel
     }
     
-    func isTwoWheeled(vehicle: Vehicle) -> Bool{
-        if let v = vehicle as? Moto{
-            if(v.hasSideCar){
+                                                //method to check whether the vehicle is two wheeled or not
+    
+    func isTwoWheeled(vehicle: Vehicle) -> Bool
+    {
+        if let v = vehicle as? Moto
+        {
+            if(v.hasSideCar)
+            {
                 return false
             }
-            else{
+            else
+            {
                 return true
             }
         }
-        else{
+        else
+        {
             return false
         }
     }
     
-    func showData(){
+                                                //method to display the deatils of Vehicle
+    
+    func showData()
+    {
         print("\(self.name) -> speed max = \(self.maxSpeed) km/h, weight = \(self.weight) kg")
     }
     
-    func better(otherVehicle: Vehicle) -> Bool{
+                                                //method to check the performance of the vehicle based on the speed and weight
+                                                //performance is calculated as the ratio of speed and weight
+                                                //better performance -> vehicle with maximum speed and is light weight
+    
+    func performance() -> Double
+    {
+        return self.maxSpeed/Double(self.weight)
+    }
+    
+                                                //method to compare the performance of one vehicle with the other vehicles
+    
+    func better(otherVehicle: Vehicle) -> Bool
+    {
         if self.performance() > otherVehicle.performance()
         {
             return true
@@ -79,11 +113,9 @@ class Vehicle: VehicleProtocol{
             return false
         }
     }
-    
-    func performance() -> Double{
-        return self.maxSpeed/Double(self.weight)
-    }
-    
 }
+
+                                                //end of super class Vehicle
+
 
 
